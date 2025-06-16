9ed96b829e843228d1d497b571cb75d9b95718b2
Maksim Bukin <oxemelot@yandex.ru>
2025-06-16 18:23:44 +0300

    Add migration for create user table

--- yii2/console/migrations/m250616_151119_create_user_table.php
+++ yii2/console/migrations/m250616_151119_create_user_table.php
@@ -2,31 +2,28 @@
 
 use yii\db\Migration;
 
-class m130524_201442_init extends Migration
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m250616_151119_create_user_table extends Migration
 {
-    public function up()
+    public function safeUp(): void
     {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            // https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
-
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
+            'verification_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
-        ], $tableOptions);
+            'created_at' => $this->dateTime()->notNull(),
+            'updated_at' => $this->dateTime(),
+        ]);
     }
 
-    public function down()
+    public function safeDown(): void
     {
         $this->dropTable('{{%user}}');
     }
