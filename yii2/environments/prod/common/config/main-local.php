4530d2c2068fe80b4827f4381b26546b7a7da77b
Maksim Bukin <oxemelot@yandex.ru>
2025-06-16 18:23:11 +0300

    Fix db component config
    Get values from environment

--- yii2/environments/prod/common/config/main-local.php
+++ yii2/environments/prod/common/config/main-local.php
@@ -4,10 +4,14 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'pgsql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+            'username' => getenv('DB_USER'),
+            'password' => getenv('DB_PASS'),
             'charset' => 'utf8',
+            'schemaMap' => ['pgsql' => [
+                'class' => 'yii\db\pgsql\Schema',
+                'defaultSchema' => getenv('DB_SCHEMA'),
+            ]],
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
